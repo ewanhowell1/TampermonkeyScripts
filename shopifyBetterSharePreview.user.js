56084dcb55e806345a33583afaee5abc6a909c54
ewanhowell1 <91874864+ewanhowell1@users.noreply.github.com>
2024-10-11 17:07:22 +0100

    Create shopifyBetterSharePreview.user.js

---
+++ shopifyBetterSharePreview.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Shopify Better Share Preview Links
+// @version      1.0
+// @description  Make Shopify "Share Preview" links link to the current page instead of the home page
+// @author       Ewan Howell
+// @match        *://*/*
+// ==/UserScript==
+
+(function() {
+  if (location.pathname !== "/preview_bar") return
+  const button = document.querySelector(".admin-bar > :first-child > ul > :nth-child(2) > button")
+  if (button) {
+    button.addEventListener("click", e => {
+      const input = document.getElementById("share_theme_url")
+      input.value += window.top.location.href.replace(window.location.origin, "")
+      console.log(window.top.location.href)
+    })
+  }
+})();
